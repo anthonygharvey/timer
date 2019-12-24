cfdf3e03f23e0c98750ce92e975343fe415e7aeb
anthonygharvey <anthony@anthonygharvey.com>
2019-12-23 22:57:40 -0500

    Add Timer callbacks to emit events outside of the Timer class

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 class Timer {
-  constructor(durationInput, startButton, pauseButton) {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
 
     this.startButton.addEventListener("click", this.start);
     this.pauseButton.addEventListener("click", this.pause);
   }
 
   start = () => {
+    if (this.onStart) this.onStart();
     this.tick(); // immediately call the tick function
 
     // set the timer to an instance variable to be accessible to other functions
@@ -22,9 +28,11 @@ class Timer {
   tick = () => {
     if (this.timeRemaining <= 0) {
       this.pause();
+      if (this.onComplete) this.onComplete();
     } else {
       // setter called 	=  getter called - decrementing
       this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) this.onTick();
     }
   };
 
@@ -41,4 +49,14 @@ const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log("Timer started");
+  },
+  onTick() {
+    console.log("Timer just ticked down");
+  },
+  onComplete() {
+    console.log("Timer completed");
+  }
+});
